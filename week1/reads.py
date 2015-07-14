5230bcddd5e039d2cae3bec2f0c0d0b564160f0a
mkhonto <jabulani@codex.co>
2015-07-14 10:08:24 +0200

    readsfiles

---
+++ week1/reads.py
@@ -0,0 +1,11 @@
+from sys import argv
+script, filename = argv
+txt = open(filename)
+print txt.read()
+#f = file.readlines(file)
+
+#for line in Monday:
+	#print line
+
+#with open(Monday) as f:
+	#content = f.readlines()
